5bd1f8bb1bf90e65cb69ec6c73e1c6bb1ea0604c
Giancarlo Papa <giancarlo.papa@gmail.com>
2022-05-10 09:04:03 +0200

    create_mariadb_admin_user table to create with mysql command no mysqladmin

--- conf/mariadb/create_mariadb_admin_user.sh
+++ conf/mariadb/create_mariadb_admin_user.sh
@@ -10,14 +10,14 @@ while [[ RET -ne 0 ]]; do
     RET=$?
 done
 
-
 PASS=${MYSQL_ADMIN_PASS:-$(< /dev/urandom tr -dc A-Z-a-z-0-9 | head -c${1:-24};echo;)}
 _word=$( [ ${MYSQL_ADMIN_PASS} ] && echo "preset" || echo "random" )
 echo "=> Creating MariaDB admin user with ${_word} password"
 
+mysql -uroot -e " CREATE IF NOT EXISTS '$MYSQL_DB_NAME'"
 mysql -uroot -e "CREATE USER '$MYSQL_ADMIN_USER'@'$MYSQL_ADMIN_HOST' IDENTIFIED BY '$PASS'"
 mysql -uroot -e "GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_ADMIN_USER'@'$MYSQL_ADMIN_HOST' WITH GRANT OPTION"
-mysql -uroot -e " CREATE IF NOT EXISTS '$MYSQL_DB_NAME'"
+
 # mysqladmin -uroot create $MYSQL_DB_NAME
 
 echo "=> Done!"
